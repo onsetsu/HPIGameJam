0a1c47888890daa6935527936964abc28491c1d4
stefan.ramson <stefan.ramson@hpi.de>
2019-03-25 18:51:17 +0100

    queue2

--- Scripts/Queue.gd
+++ Scripts/Queue.gd
@@ -5,10 +5,24 @@ var person_scene = preload("res://Scenes/Person.tscn")
 
 var entities = []
 
+var path_length
+
 func _ready():
-    for i in range(0,11):
-        var person = person_scene.instance()
-        $entities.add_child(person)
+    path_length = $path.curve.get_baked_length()
+    
+    for i in range(0,25):
+        var entity
+        if i % 5 == 0:
+            entity = sheep_scene.instance()
+        else:
+            entity = person_scene.instance()
+        $path
+        var follow = PathFollow2D.new()
+        follow.rotate = false
+        follow.offset = (path_length - 100 ) * i / 30.0
+        $path.add_child(follow)
+        follow.add_child(entity)
+
 func _process(delta):
-    $"path/path-follow".offset += 30 * delta
-    print($"path/path-follow/Sheep".transform.get_rotation())
+    for follow_path in $path.get_children():
+        follow_path.offset += 30 * delta
