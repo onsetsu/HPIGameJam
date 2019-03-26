a8ea18bf7a822c8cd709a6b786ae4b818082547b
Vaskulis <vaskulis@posteo.de>
2019-03-26 16:03:14 +0100

    finished ingredients

--- Scripts/Ingredients.gd
+++ Scripts/Ingredients.gd
@@ -3,10 +3,7 @@ extends Node
 var sheep_scene = preload("res://Scenes/Sheep.tscn")
 
 func _ready():
-	var sh = sheep_scene.instance()
-	sh.fur_color = sh.FUR_COLOR.blue
-	print(_can_be_eaten(sh, "Rindfleisch"))
-	
+	pass
 
 static func can_be_eaten(sheep, ingredient):
 	var fc = sheep.fur_color
@@ -39,5 +36,29 @@ static func can_be_eaten(sheep, ingredient):
 	elif ingredient == "Rosenkohl":
 		if (not hc==e_hc.pink):
 			return false
+	elif ingredient == "Curry":
+		if pt==e_pt.dots and (not hc==e_hc.black):
+			return false
+	elif ingredient == "Ei":
+		if (fc==e_fc.orange and hc==e_hc.black) or (pt==e_pt.stripes and hc==e_hc.white):
+			return false
+	elif ingredient == "Alkohol":
+		if (not fc==e_fc.green) and (pt==e_pt.dots or pt==e_pt.stripes):
+			return false
+	elif ingredient == "Knoblauch":
+		if hc==e_hc.pink and pt==e_pt.none:
+			return false
+	elif ingredient == "Pilze":
+		if fc==e_fc.blue and hc==e_hc.white:
+			return false
+	elif ingredient == "Senf":
+		if pt==e_pt.stripes:
+			return false
+	elif ingredient == "Soja":
+		if (not fc==e_fc.orange) and (not hc==e_hc.black):
+			return false
+	else:
+		print("ERROR! Zutat " + ingredient + " ist nicht definiert!")
+		return null
 	return true
 
